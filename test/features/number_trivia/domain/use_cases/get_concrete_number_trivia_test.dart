f1273213f355e1c7e499ce360672a8c03385ef63
Oscar Pallazhco <oepallaz@gmail.com>
2022-02-02 13:48:01 -0500

    test, fail because  use case GetConcreteNumberTrivia is not already created

---
+++ test/features/number_trivia/domain/use_cases/get_concrete_number_trivia_test.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_tdd_clean_architecturre/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+class MockNumberTriviaRepository extends Mock
+    implements NumberTriviaRepository {}
+
+void main() {
+  GetConcreteNumberTrivia usecase;
+  MockNumberTriviaRepository mockNumberTriviaRepository;
+
+  setUp(() {
+    mockNumberTriviaRepository = MockNumberTriviaRepository();
+    usecase = GetConcreteNumberTrivia(mockNumberTriviaRepository);
+  });
+}
