b605d549a24f06d4c6e2c9c35a391d7f42714570
Oscar Pallazhco <oepallaz@gmail.com>
2022-02-02 13:56:24 -0500

    not fail for not exist usecase

--- test/features/number_trivia/domain/use_cases/get_concrete_number_trivia_test.dart
+++ test/features/number_trivia/domain/use_cases/get_concrete_number_trivia_test.dart
@@ -1,6 +1,8 @@
-import 'package:flutter_tdd_clean_architecturre/features/number_trivia/domain/repositories/number_trivia_repository.dart';
-import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
+
+import 'package:flutter_tdd_clean_architecturre/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+import 'package:flutter_tdd_clean_architecturre/features/number_trivia/domain/use_cases/get_concrete_number_trivia.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 class MockNumberTriviaRepository extends Mock
     implements NumberTriviaRepository {}
