28007bb8b73d1c7b4446f8f177dbb8060a4b9b8a
Oscar Pallazhco <oepallaz@gmail.com>
2022-02-06 22:50:55 -0500

    test: NumberTriviaModel should be a subclass of NumberTrivia entity

---
+++ test/features/number_trivia/data/models/number_trivia_model_test.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:flutter_tdd_clean_architecturre/features/number_trivia/domain/entities/number_trivia.dart';
+
+void main() {
+  final tNUmberTriviaModel = NumberTriviaModel(text: 'text', number: 1);
+
+  test(
+    'NumberTriviaModel should be a subclass of NumberTrivia entity',
+    () async {
+      // assert
+      expect(tNUmberTriviaModel, isA<NumberTrivia>())
+    },
+  );
+}
