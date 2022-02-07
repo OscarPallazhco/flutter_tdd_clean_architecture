a9d7500be2b47e76e61fe3249bb6560deba86784
Oscar Pallazhco <oepallaz@gmail.com>
2022-02-06 23:39:52 -0500

    test for NumberTriviaModel.fromJson

--- test/features/number_trivia/data/models/number_trivia_model_test.dart
+++ test/features/number_trivia/data/models/number_trivia_model_test.dart
@@ -1,6 +1,8 @@
-import 'package:flutter_tdd_clean_architecturre/features/number_trivia/data/models/number_trivia_model.dart';
-import 'package:flutter_test/flutter_test.dart';
+import 'dart:convert';
 
+import '../../fixtures/fixture_reader.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_tdd_clean_architecturre/features/number_trivia/data/models/number_trivia_model.dart';
 import 'package:flutter_tdd_clean_architecturre/features/number_trivia/domain/entities/number_trivia.dart';
 
 void main() {
@@ -13,4 +15,21 @@ void main() {
       expect(tNUmberTriviaModel, isA<NumberTrivia>());
     },
   );
+
+  group("fromJson", () {
+    test(
+      'should be return a valid model when the JSON number is an integer',
+      () async {
+        // arrange
+        final Map<String, dynamic> jsonMap =
+            json.decode(fixture('trivia.json'));
+
+        // act
+        final result = NumberTriviaModel.fromJson(jsonMap);
+
+        // assert
+        expect(result, tNUmberTriviaModel);
+      },
+    );
+  });
 }
