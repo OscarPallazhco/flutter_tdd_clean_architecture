3d91bcbadf26e6d4691b50c6905fcd246f892553
Oscar Pallazhco <oepallaz@gmail.com>
2022-02-22 10:10:14 -0500

    test use of DataConnectionChecker inside the method isConnected in NetworkInfoImpl class

---
+++ test/core/network/network_info_test.dart
@@ -0,0 +1,40 @@
+import 'package:data_connection_checker_tv/data_connection_checker.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:flutter_tdd_clean_architecturre/core/network/network_info.dart';
+import 'network_info_test.mocks.dart';
+
+
+@GenerateMocks([DataConnectionChecker])
+void main() {
+
+  late NetworkInfoImpl networkInfoImpl;
+  late MockDataConnectionChecker mockDataConnectionChecker;
+  
+  setUp((){
+    mockDataConnectionChecker = MockDataConnectionChecker();
+    networkInfoImpl= NetworkInfoImpl(mockDataConnectionChecker);
+  });
+
+  group('isConnected', (){
+
+    test(
+      'should forward the call to DataConnectionChecker.hasConnection',
+      () async {
+        // arrange
+        when(mockDataConnectionChecker.hasConnection).thenAnswer((_) async => true);
+    
+        // act
+        final result = await networkInfoImpl.isConnected;
+    
+        // assert
+        verify(mockDataConnectionChecker.hasConnection);
+        expect(result, true);
+      },
+    );
+
+  });
+
+}
