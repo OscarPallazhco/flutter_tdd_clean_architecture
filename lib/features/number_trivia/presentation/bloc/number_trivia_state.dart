3aa6e029e3cd81c4e9041b6734753dafd324b2e5
Oscar Pallazhco <oepallaz@gmail.com>
2022-03-16 19:56:19 -0500

    states for number_trivia bloc

--- lib/features/number_trivia/presentation/bloc/number_trivia_state.dart
+++ lib/features/number_trivia/presentation/bloc/number_trivia_state.dart
@@ -2,9 +2,29 @@ part of 'number_trivia_bloc.dart';
 
 abstract class NumberTriviaState extends Equatable {
   const NumberTriviaState();
-  
+
   @override
   List<Object> get props => [];
 }
 
-class NumberTriviaInitial extends NumberTriviaState {}
+class Empty extends NumberTriviaState {}
+
+class Loading extends NumberTriviaState {}
+
+class Loaded extends NumberTriviaState {
+  final NumberTrivia numberTrivia;
+
+  Loaded({required this.numberTrivia}) : super();
+
+  // @override
+  // List<Object> get props => [numberTrivia];
+}
+
+class Error extends NumberTriviaState {
+  final String message;
+
+  Error({required this.message}) : super();
+
+  // @override
+  // List<Object> get props => [message];
+}
