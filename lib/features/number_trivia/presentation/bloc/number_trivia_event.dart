64e9d8cacf0730886a32d9f09ce4618d64b9217a
Oscar Pallazhco <oepallaz@gmail.com>
2022-03-16 19:19:33 -0500

    initial number_trivia bloc

---
+++ lib/features/number_trivia/presentation/bloc/number_trivia_event.dart
@@ -0,0 +1,18 @@
+part of 'number_trivia_bloc.dart';
+
+abstract class NumberTriviaEvent extends Equatable {
+  NumberTriviaEvent([List props = const <dynamic>[]]):super();
+
+  @override
+  List<Object> get props => [];
+}
+
+class GetTriviaForConcreteNumber extends NumberTriviaEvent {
+  final String numberString;
+  GetTriviaForConcreteNumber(this.numberString):super();
+}
+
+class GetTriviaForRandomNumber extends NumberTriviaEvent {
+}
+
+
