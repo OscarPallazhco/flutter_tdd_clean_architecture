b3d3fe2312c6d6924b640548051216fc4fa0078e
Oscar Pallazhco <oepallaz@gmail.com>
2022-03-16 19:20:16 -0500

    test stringToUnsignedInteger

---
+++ test/core/util/input_converter_test.dart
@@ -0,0 +1,27 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_tdd_clean_architecturre/core/util/input_converter.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  late InputConverter inputConverter;
+
+  setUp(() {
+    inputConverter = InputConverter();
+  });
+
+  group('stringToUnsignedInt', () {
+    test(
+      'should retun an integer when the string represents an unsigned integer.',
+      () async {
+        // arrange
+        final str = '123';
+
+        // act
+        final result = inputConverter.stringToUnsignedInteger(str);
+
+        // assert
+        expect(result, Right(123));
+      },
+    );
+  });
+}
