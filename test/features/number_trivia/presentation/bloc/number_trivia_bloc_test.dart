4f1dcf2291cc343f3f2ab8820ec4ebbbc6ed5f86
Oscar Pallazhco <oepallaz@gmail.com>
2022-03-21 22:45:09 -0500

    test NumberTriviaBloc Should call  InputConvert

--- test/features/number_trivia/presentation/bloc/number_trivia_bloc_test.dart
+++ test/features/number_trivia/presentation/bloc/number_trivia_bloc_test.dart
@@ -1,5 +1,6 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_tdd_clean_architecturre/core/util/input_converter.dart';
+import 'package:flutter_tdd_clean_architecturre/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:flutter_tdd_clean_architecturre/features/number_trivia/domain/use_cases/get_concrete_number_trivia.dart';
 import 'package:flutter_tdd_clean_architecturre/features/number_trivia/domain/use_cases/get_random_number_trivia.dart';
 import 'package:flutter_tdd_clean_architecturre/features/number_trivia/presentation/bloc/number_trivia_bloc.dart';
@@ -17,7 +18,7 @@ void main() {
   late NumberTriviaBloc bloc;
   MockGetConcreteNumberTrivia mockGetConcreteNumberTrivia;
   MockGetRandomNumberTrivia mockGetRandomNumberTrivia;
-  MockInputConverter mockInputConverter;
+  late MockInputConverter mockInputConverter;
 
   setUp(() {
     mockGetConcreteNumberTrivia = MockGetConcreteNumberTrivia();
@@ -37,4 +38,25 @@ void main() {
     },
   );
 
+  group('GetTriviaForConcreteNumber', () {
+    final tNumberString = '1';
+    final tNumberParsed = 1;
+    final tNumberTrivia = NumberTrivia(text: "test trivia", number: tNumberParsed);
+
+    test(
+      'Should call the InputConvert to validate and convert the string to an unsigned integer',
+      () async {
+        // arrange
+        when(mockInputConverter.stringToUnsignedInteger(any as String)).thenReturn(Right(tNumberParsed));
+    
+        // act
+        bloc.add((GetTriviaForConcreteNumber(tNumberString)));
+        await untilCalled(mockInputConverter.stringToUnsignedInteger(any as String));
+    
+        // assert
+        verify(mockInputConverter.stringToUnsignedInteger(tNumberString));
+      },
+    );
+
+  });
 }
